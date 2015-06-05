85526f17446ecd8b20019faa2423a2e0ade87dee
川上慶士 <keiji_kangouchi@yahoo.co.jp>
2015-06-05 16:29:38 +0900

    初期状態

---
+++ AppRecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppRecord
+//
+//  Created by 川上慶士 on 2015/06/05.
+//  Copyright (c) 2015年 川上慶士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
